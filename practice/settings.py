5f09ccef9986e6af2fb3e8bf97ad4baf5afeeb08
Mateus Barbosa Lopes <mateusbarbosalopes999@gmail.com>
2024-06-11 15:29:58 -0300

    message

--- practice/settings.py
+++ practice/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yrwoh5yv_4srg=uhxdw9(6iwiqz%y%x2nwwi5fvdmh_n)t6vy&'
+SECRET_KEY = 'django-insecure-udcjes_61$%#9&+ftoio$ug2$cj4+8^t8*e85053h!rh655z)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
