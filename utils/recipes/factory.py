e9ee1a3db6d39f8aa8ac7071aec4ad8c5f01e7a2
Mateus Barbosa <mateusbarbosa999@gmail.com>
2024-12-12 09:00:01 -0300

    refactor: Update recipe.html and factory.py for improved template rendering and data generation

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -1,15 +1,10 @@
+# from inspect import signature
 from random import randint
-
 from faker import Faker
-
-
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
-
-
-fake = Faker('pt-BR')
-
-
+fake = Faker('pt_BR')
+# print(signature(fake.random_number))
 def make_recipe():
     return {
         'title': fake.sentence(nb_words=6),
@@ -19,8 +14,8 @@ def make_recipe():
         'servings': fake.random_number(digits=2, fix_len=True),
         'servings_unit': 'Porção',
         'preparation_steps': fake.text(3000),
-        'created_at_': fake.date_time(),
-        'autor': {
+        'created_at': fake.date_time(),
+        'author': {
             'first_name': fake.first_name(),
             'last_name': fake.last_name(),
         },
@@ -28,11 +23,9 @@ def make_recipe():
             'name': fake.word()
         },
         'cover': {
-            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio()
-        },
-    },
-
-
+            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
+        }
+    }
 if __name__ == '__main__':
     from pprint import pprint
     pprint(make_recipe())
