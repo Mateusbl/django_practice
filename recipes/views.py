0534acd85395bb721cbfec8f32081585251d110a
Mateus Barbosa <mateusbarbosa999@gmail.com>
2026-03-20 10:06:46 -0300

    feat: filtro para is published

--- recipes/views.py
+++ recipes/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+
 from recipes.models import Recipe
 
 
-
 def home(request):
-    recipes = Recipe.objects.all().order_by('-created_at')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "recipes": recipes,
+        },
+    )
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
-        'is_detail_page': True,
-    })
-
-
+    return render(
+        request,
+        "recipes/pages/recipe-view.html",
+        context={
+            "recipe": make_recipe(),
+            "is_detail_page": True,
+        },
+    )
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-created_at')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by("-created_at")
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "recipes": recipes,
+        },
+    )
