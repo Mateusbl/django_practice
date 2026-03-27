5e1ab8d8df9cace55a33ed8c825e9d2bc6dd566e
Mateus Barbosa <mateusbarbosa999@gmail.com>
2026-03-27 20:07:34 -0300

    fix: update category view to use get_list_or_404 for better error handling and fix title assignment

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from django.http import Http404
 from recipes.models import Recipe
@@ -27,13 +27,12 @@ def recipe(request, id):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True
-    ).order_by("-created_at")
-    first_recipe = recipes.first()
-    if first_recipe is None:
-        raise Http404("Not found")
-
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by("-id")
+    )
     return render(
         request,
         "recipes/pages/category.html",
